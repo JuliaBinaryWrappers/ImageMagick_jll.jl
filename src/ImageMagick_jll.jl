6b731aa159bd3227914f5feb4b427a1dc2addf18
Elliot Saba <staticfloat@gmail.com>
2019-09-18 13:10:30 -0700

    ImageMagick_jll build 6.9.10-12+0

---
+++ src/ImageMagick_jll.jl
@@ -0,0 +1,31 @@
+module ImageMagick_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("c73af94c-d91f-53ed-93a7-00f77d67a9d7"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "ImageMagick", artifacts_toml) for e in artifacts["ImageMagick"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load ImageMagick; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module ImageMagick_jll
