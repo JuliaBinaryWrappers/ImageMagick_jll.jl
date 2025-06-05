844d5c79fd5f499fb637353ab499b225dd445331
jlbuild <juliabuildbot@gmail.com>
2025-06-05 22:19:06 +0000

    ImageMagick_jll build 7.1.1048+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -8,7 +8,7 @@ using OpenJpeg_jll
 using Zlib_jll
 using libpng_jll
 JLLWrappers.@generate_wrapper_header("ImageMagick")
-JLLWrappers.@declare_library_product(libwand, "@rpath/libMagickWand-6.Q16.7.dylib")
+JLLWrappers.@declare_library_product(libwand, "@rpath/libMagickWand-7.Q16HDRI.10.dylib")
 JLLWrappers.@declare_executable_product(identify)
 JLLWrappers.@declare_executable_product(imagemagick_convert)
 JLLWrappers.@declare_executable_product(mogrify)
@@ -17,7 +17,7 @@ function __init__()
     JLLWrappers.@generate_init_header(Ghostscript_jll, JpegTurbo_jll, Libtiff_jll, OpenJpeg_jll, Zlib_jll, libpng_jll)
     JLLWrappers.@init_library_product(
         libwand,
-        "lib/libMagickWand-6.Q16.7.dylib",
+        "lib/libMagickWand-7.Q16HDRI.10.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
